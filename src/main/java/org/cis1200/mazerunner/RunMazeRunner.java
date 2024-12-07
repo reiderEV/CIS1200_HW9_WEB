ca6841db76f18ce97e44686d939b5aa462729795
reiderEV <126197172+reiderEV@users.noreply.github.com>
2024-12-07 12:20:12 -0500

    new starter level

--- src/main/java/org/cis1200/mazerunner/RunMazeRunner.java
+++ src/main/java/org/cis1200/mazerunner/RunMazeRunner.java
@@ -10,7 +10,7 @@ public class RunMazeRunner implements Runnable {
     @Override
     public void run() {
         try {
-            GameLogic game = new GameLogic("files/mazes/easy10_10.txt");
+            GameLogic game = new GameLogic("files/mazes/hard20_30.txt");
 
             JFrame frame = new JFrame("Maze Runner");
             frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
